c9381862e70149854b85e0d382f287cd7fc57483
jSalanesh <joseph.sala2001@gmail.com>
2025-03-13 08:39:12 +0800

    Version 0.1

--- workhelper.py
+++ workhelper.py
@@ -1,14 +1,24 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request, redirect,flash,session
 
 app = Flask(__name__)
+app.secret_key = "Wakatabotobilat251"
 
-@app.route("/")
+@app.route("/",methods=["POST","GET"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        base_num = request.form["baseNum"]
+        flash(base_num,"info")
+        return redirect(url_for("home"))
+    else:
+        return render_template("index.html")
 
 @app.route("/dailytask")
 def dailyTask():
     return render_template("dailytask.html")
+
+@app.route("/actionitems")
+def actionItems():
+    return render_template("actionitems.html")
 
 @app.route("/roreview")
 def repairOrderReview():
